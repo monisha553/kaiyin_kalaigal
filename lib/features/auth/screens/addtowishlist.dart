430f3c189c01a456d30d91885f7a42a506c7d6c2
monisha553 <monisha.munaswamy@gmail.com>
2024-04-15 18:38:53 +0530

    third commet

---
+++ lib/features/auth/screens/addtowishlist.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class AddToWishlish extends StatefulWidget {
+  const AddToWishlish({super.key});
+
+  @override
+  State<AddToWishlish> createState() => _AddToWishlishState();
+}
+
+class _AddToWishlishState extends State<AddToWishlish> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
